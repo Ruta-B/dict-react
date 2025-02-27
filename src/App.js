6dc3ec3fe67ebd4fe41be62b8ce0a872738e46b8
Ruta-B <baumaneruta38@gmail.com>
2025-02-27 09:55:34 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,15 +5,23 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">R.B</header>
-        <footer>
+      <footer>
         <a
           href="https://github.com/Ruta-B/dict-react"
           target="_blank"
           rel="noopener noreferrer"
         >
-         Github
-        </a></footer>
-      
+          Github
+        </a>{" "}
+        and{" "}
+        <a
+          href="https://dictreact.netlify.app/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Netlify{" "}
+        </a>
+      </footer>
     </div>
   );
 }
