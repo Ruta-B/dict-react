17424271651b88a1cd9651f7cb0b7e01292c50ee
Ruta-B <baumaneruta38@gmail.com>
2025-02-27 09:51:46 +0100

    ..

--- src/App.js
+++ src/App.js
@@ -4,15 +4,16 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="App-header">R.B</header>
+        <footer>
         <a
           href="https://github.com/Ruta-B/dict-react"
           target="_blank"
           rel="noopener noreferrer"
         >
          Github
-        </a>
-      </header>
+        </a></footer>
+      
     </div>
   );
 }
